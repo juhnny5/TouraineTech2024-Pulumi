f9de65702efe3496c14e1c41d409e00e5c5d1b2d
Julien Briault <jbriault@deezer.com>
2024-01-31 14:58:36 +0100

    Update code 😎

--- lib/vm.sh
+++ lib/vm.sh
@@ -1,6 +1,9 @@
 vm_name="minio"
 multipass launch --name ${vm_name} --cpus 2 --mem 4G --disk 20G
 multipass stop ${vm_name}
+
+# Astuce macOS pour le problème de NAT
 sudo pfctl -f /etc/pf.conf
 sudo pfctl -s all
+
 multipass start ${vm_name}
