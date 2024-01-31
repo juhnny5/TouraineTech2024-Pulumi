992ea583c780697f9d3e8ebde1d4ae3d4da53c2c
Julien Briault <jbriault@deezer.com>
2024-01-31 15:32:27 +0100

    Update code 😎

--- lib/install_minio.sh
+++ lib/install_minio.sh
@@ -1,12 +1,12 @@
 deb_package_name="minio_20240129035632.0.0_arm64.deb"
 wget https://dl.minio.io/server/minio/release/linux-arm64/${deb_package_name}
 sudo dpkg -i ${deb_package_name}
-sudo groupadd -r minio
-sudo useradd -M -r -g minio minio -s /sbin/nologin
+sudo groupadd -r minio-user
+sudo useradd -M -r -g minio-user minio-user -s /sbin/nologin
 sudo mkdir /usr/local/share/minio
-sudo chown minio: /usr/local/share/minio
+sudo chown minio-user: /usr/local/share/minio
 sudo mkdir /etc/minio
-sudo chown minio: /etc/minio
+sudo chown minio-user: /etc/minio
 sudo echo 'MINIO_ACCESS_KEY="minio"
 MINIO_VOLUMES="/usr/local/share/minio/"
 MINIO_OPTS="-C /etc/minio --address 0.0.0.0:9000"
