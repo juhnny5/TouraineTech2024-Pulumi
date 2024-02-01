80c80c2816a2e3cde86ff1bafc8e6e9e79989654
Julien Briault <jbriault@deezer.com>
2024-02-01 18:10:06 +0100

    Update code 😎

--- setup_env.sh
+++ setup_env.sh
@@ -6,8 +6,8 @@ source lib/requirements.sh
 # Initialiser la VM
 source lib/vm.sh
 
-mkdir -p ~/.aws/credentials
+# mkdir -p ~/.aws/credentials
 
-echo "[minio-demo]
-aws_access_key_id = minio
-aws_secret_access_key = minio" >> ~/.aws/credentials
+# echo "[minio-demo]
+# aws_access_key_id = minio
+# aws_secret_access_key = minio" >> ~/.aws/credentials
