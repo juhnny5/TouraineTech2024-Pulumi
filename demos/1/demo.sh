05120e35e5801251938ec7dc0449dff13b738cc0
Julien Briault <jbriault@deezer.com>
2024-02-03 14:54:05 +0100

    Update code 😎

--- demos/1/demo.sh
+++ demos/1/demo.sh
@@ -3,4 +3,9 @@
 # Original script: curl -LO https://raw.githubusercontent.com/paxtonhare/demo-magic/master/demo-magic.sh
 . lib/demo_magic.sh
 
-p "pulumi login \"s3://plstate-openstack-demo1?endpoint=${VM_IP}:9000&disableSSL=true&s3ForcePathStyle=true\""
+# Store state in MinIO
+pe "pulumi login \"s3://plstate-openstack-demo1?endpoint=${VM_IP}:9000&disableSSL=true&s3ForcePathStyle=true\""
+
+pe "pulumi preview --non-interactive"
+
+pe "pulumi update --non-interactive --yes"
