6ae1b2431510cf93657c0caf9c5524d5587f6091
Julien Briault <jbriault@deezer.com>
2024-02-03 18:58:32 +0100

    Update code 😎

--- demos/4/demo.sh
+++ demos/4/demo.sh
@@ -12,6 +12,11 @@ DEMO_PROMPT="${BLUE}demo-4$ ${COLOR_RESET}"
 DEMO_COMMENT_COLOR=$CYAN
 
 # Store state in MinIO
+pei "# Stocker le state dans MinIO"
 pe "pulumi login \"s3://plstate-openstack-demo4?endpoint=${VM_IP}:9000&disableSSL=true&s3ForcePathStyle=true\""
+pei "# Afficher les modifications à apporter"
 pe "pulumi preview --non-interactive"
+pei "# Appliquer les modifications à apporter"
 pe "pulumi update --non-interactive --yes"
+pei "# Convertir le projet de YAML vers Python"
+p "pulumi convert  --language python"
