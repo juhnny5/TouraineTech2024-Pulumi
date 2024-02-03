16b0365555cb84f5eb020a67308843e9a93126eb
Julien Briault <jbriault@deezer.com>
2024-02-03 15:08:20 +0100

    Update code 😎

--- demos/1/demo.sh
+++ demos/1/demo.sh
@@ -6,6 +6,11 @@
 source ../../.env
 source $HOME/.keystonerc
 
+TYPE_SPEED=50 # Accelerate typing
+DEMO_CMD_COLOR="" # No bold
+DEMO_PROMPT="${BLUE}$ ${COLOR_RESET}"
+DEMO_COMMENT_COLOR=$CYAN
+
 # Store state in MinIO
 pe "pulumi login \"s3://plstate-openstack-demo1?endpoint=${VM_IP}:9000&disableSSL=true&s3ForcePathStyle=true\""
 pe "pulumi preview --non-interactive"
