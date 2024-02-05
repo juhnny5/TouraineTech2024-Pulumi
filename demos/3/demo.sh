5210f2cc5c27a0c89f500d3743e04a545f14ece2
Julien Briault <jbriault@deezer.com>
2024-02-05 15:00:08 +0100

    Update code 😎

---
+++ demos/3/demo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Original script: curl -LO https://raw.githubusercontent.com/paxtonhare/demo-magic/master/demo-magic.sh
+. ../../lib/demo_magic.sh
+
+source ../../.env
+source $HOME/.keystonerc
+
+TYPE_SPEED=50 # Accelerate typing
+DEMO_CMD_COLOR="" # No bold
+DEMO_PROMPT="${BLUE}demo-3$ ${COLOR_RESET}"
+DEMO_COMMENT_COLOR=$CYAN
+
+# Store state in MinIO
+pei "# Stocker le state dans MinIO"
+pe "pulumi login \"s3://plstate-openstack-demo3?endpoint=${VM_IP}:9000&disableSSL=true&s3ForcePathStyle=true\""
+pei "# Afficher les modifications à apporter"
+pe "pulumi preview --non-interactive"
+pei "# Appliquer les modifications à apporter"
+pe "pulumi update --non-interactive --yes"
